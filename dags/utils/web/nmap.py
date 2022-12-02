2a20b5bde438d51245b97f8373083625f827829a
Ferran <ferran.obiols.colomina@gmail.com>
2022-12-02 15:15:33 +0100

    Add scripts for the nmap and modified the report for open web servers

--- dags/utils/web/nmap.py
+++ dags/utils/web/nmap.py
@@ -9,20 +9,105 @@ class Nmap:
         self.ssh = SecureShell('web-tools', 'root', '/opt/airflow/config/id_rsa')
         self.results_path = results
         self.default_web_ports = ['80', '443', '8080', '8443']
-        self.common_web_ports = ['66', '80', '81', '443', '445', '457', '1080', '1100', '1241', '1352', '1433', '1434', '1521', '1944', '2301', '3000', '3128', '3306', '4000', '4001', '4002', '4100', '5000', '5432', '5800', '5801', '5802', '6346', '6347', '7001', '7002', '8000', '8080', '8181', '8443', '8888', '30821']
-        self.web_scripts = []
-
+        self.common_web_ports = [
+            '66',
+            '80',
+            '81',
+            '443',
+            '445',
+            '457',
+            '1080',
+            '1100',
+            '1241',
+            '1352',
+            '1433',
+            '1434',
+            '1521',
+            '1944',
+            '2301',
+            '3000',
+            '3128',
+            '3306',
+            '4000',
+            '4001',
+            '4002',
+            '4100',
+            '5000',
+            '5432',
+            '5800',
+            '5801',
+            '5802',
+            '6346',
+            '6347',
+            '7001',
+            '7002',
+            '8000',
+            '8080',
+            '8181',
+            '8443',
+            '8888',
+            '30821'
+        ]
+        self.web_scripts = [
+            'http-adobe-coldfusion-apsa1301.nse',
+            'http-apache-server-status.nse',
+            'http-aspnet-debug.nse',
+            'http-bigip-cookie.nse',
+            'http-cakephp-version.nse',
+            'http-cisco-anyconnect.nse',
+            'http-coldfusion-subzero.nse',
+            'http-cors.nse',
+            'http-frontpage-login.nse',
+            'http-headers.nse',
+            'http-iis-webdav-vuln.nse',
+            'http-internal-ip-disclosure.nse',
+            'http-litespeed-sourcecode-download.nse',
+            'http-majordomo2-dir-traversal.nse',
+            'http-mcmp.nse',
+            'http-methods.nse',
+            'http-ntlm-info.nse',
+            'http-php-version.nse',
+            'http-robots.txt.nse',
+            'http-shellshock.nse',
+            'http-trace.nse',
+            'http-vmware-path-vuln.nse',
+            'http-vuln-cve2006-3392.nse',
+            'http-vuln-cve2009-3960.nse',
+            'http-vuln-cve2010-0738.nse',
+            'http-vuln-cve2010-2861.nse',
+            'http-vuln-cve2011-3192.nse',
+            'http-vuln-cve2011-3368.nse',
+            'http-vuln-cve2012-1823.nse',
+            'http-vuln-cve2013-0156.nse',
+            'http-vuln-cve2013-6786.nse',
+            'http-vuln-cve2013-7091.nse',
+            'http-vuln-cve2014-2126.nse',
+            'http-vuln-cve2014-2127.nse',
+            'http-vuln-cve2014-2128.nse',
+            'http-vuln-cve2014-2129.nse',
+            'http-vuln-cve2014-3704.nse',
+            'http-vuln-cve2014-8877.nse',
+            'http-vuln-cve2015-1427.nse',
+            'http-vuln-cve2015-1635.nse',
+            'http-vuln-cve2017-1001000.nse',
+            'http-vuln-cve2017-5638.nse',
+            'http-vuln-cve2017-5689.nse',
+            'http-vuln-cve2017-8917.nse',
+            'http-waf-detect.nse',
+            'http-waf-fingerprint.nse',
+            'http-webdav-scan.nse'
+        ]
+        
     def nmap_web_fast(self, host, ports):
         if ports:
             scan_ports = ','.join(ports)
         else:
             scan_ports = ','.join(self.common_web_ports)
 
-        normal_result = join(self.results_path, 'find_web_servers.nmap')
+        normal_result = join(self.results_path, 'nmap_web_fast.nmap')
         out = self.ssh.execute_wait_command(f'nmap -sT -sV -T4 -oN {normal_result} -oG - -p {scan_ports} {host}')
-        return ' '.join(map(str, out['stdout'].readlines()))
-
-    #TODO: filter http scripts   
+        return ''.join(map(str, out['stdout'].readlines()))
+ 
     def nmap_web_scripts(self, host, ports):
         if not ports:
             ports = ','.join(self.default_web_ports)
@@ -30,22 +115,22 @@ class Nmap:
         normal_result = join(self.results_path, 'nmap_web_scripts.nmap')
         stdin, stdout, stderr = self.ssh.execute_command(f'nmap -sT -sV --script {",".join(self.web_scripts)} -oN {normal_result} -p {ports} {host}')
 
-    #TODO: generate good report for open web servers
     def analyze_webservers(self, gnmap):
-        open_ports = findall(r'([0-9]+)/open/tcp//http?[0-9a-zA-Z-_]+/', gnmap)
+        res1 = findall(r'Host: ([0-9a-zA-Z_\s\-\.]+) \(([0-9a-zA-Z_\s\-\.]+)?\)\sPorts:', gnmap)
+        res2 = findall(r'([0-9]+)/open/tcp//http([0-9a-zA-Z_\s\-\.\(\)]+)?//([0-9a-zA-Z_\s\-\.\(\)]+)/', gnmap)
         
-        host = "localhost"
-        if open_ports:
-            ports_string = ''.join([f'- {host}:{p}\n' for p in open_ports])
+        if res1 and res2:
+            host = res1[0][0]
+            webs = ''.join([f'{host}:{x[0]} [{x[2]}]\n' for x in res2])
             results = {
                 'tpye': 'recon',
-                'module': 'find_web_servers',
+                'module': 'web_servers',
                 'description': 'Find alternative web servers on the target machine. These alternative web servers could run old versions or a pre-production environment with new vulnerabilities or sensitive information.',
                 'risk': 'INFORMATIVE',
                 'cvss': 0,
-                'result': f'It has been found the following HTTP servers in the remote host:\n{ports_string}',
+                'result': f'It has been found the following HTTP servers in the remote host:\n{webs}',
             }
             
-            with open(join(self.results_path, 'find_web_servers.results'), 'w') as f:
+            with open(join(self.results_path, 'web_servers.results'), 'w') as f:
                 dump(results, f)        
     
