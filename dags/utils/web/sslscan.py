300cbe65b63fa6b9af8184a3ea27448bee0f32ea
Ferran <ferran.obiols.colomina@gmail.com>
2023-01-01 21:39:21 +0100

    Rework for SSL/TLS tools (TODO: analyzers)

--- dags/utils/web/sslscan.py
+++ dags/utils/web/sslscan.py
@@ -3,11 +3,13 @@ from utils.common.ssh import SecureShell
 from os.path import join
 
 class Sslscan:
-    def __init__(self, results):
+    def __init__(self, results: str) -> None:
         self.ssh = SecureShell('web-tools', 'root', '/opt/airflow/config/id_rsa')
         self.results_path = results
 
-    def sslscan_default(self, url):
+    #Launch default sslscan. Return the results in XML format.
+    def run_sslscan_default(self, url: str) -> str:
         path_out = join(self.results_path, 'sslscan_default.txt')
         path_xml = join(self.results_path, 'sslscan_default.xml')
-        stdin, stdout, stderr = self.ssh.execute_command(f'sslscan --xml={path_xml} {url} | tee {path_out}')
+        out = self.ssh.execute_wait_command(f'sslscan --xml={path_xml} {url} | tee {path_out}')
+        return path_xml
--- dags/utils/web/testssl.py
+++ dags/utils/web/testssl.py
@@ -3,11 +3,13 @@ from utils.common.ssh import SecureShell
 from os.path import join
 
 class Testssl:
-    def __init__(self, results):
+    def __init__(self, results: str) -> None:
         self.ssh = SecureShell('web-tools', 'root', '/opt/airflow/config/id_rsa')
         self.results_path = results
 
-    def testssl_default(self, url):
-        path_out = join(self.results_path, 'default.testssl')
-        path_json = join(self.results_path, 'default_json.testssl')
-        stdin, stdout, stderr = self.ssh.execute_command(f'testssl -oL {path_out} -oJ {path_json} {url}')
+    #Launch default testssl scan. Return the results in JSON format.
+    def run_testssl_default(self, url: str) -> str:
+        path_out = join(self.results_path, 'testssl_default.txt')
+        path_json = join(self.results_path, 'testssl_default.json')
+        out = self.ssh.execute_wait_command(f'testssl -oL {path_out} -oJ {path_json} {url}')
+        return path_json
