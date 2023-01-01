1095cd1aa6b9798bf5459b34260cbd169a1d1bac
Ferran <ferran.obiols.colomina@gmail.com>
2023-01-01 20:41:08 +0100

    Rework dirb wrapper (TODO: sensitive analyzer).

--- dags/utils/web/dirb.py
+++ dags/utils/web/dirb.py
@@ -2,15 +2,20 @@ from utils.common.ssh import SecureShell
 
 from os.path import join
 
-class Gobuster:
-    def __init__(self):
+class Dirb:
+    def __init__(self, results: str) -> None:
         self.ssh = SecureShell('web-tools', 'root', '/opt/airflow/config/id_rsa')
+        self.results_path = results
 
-    def dir_enum(self, url, path):        
-        path_out = join(path, 'direnum.dirb')
-        stdin, stdout, stderr = self.ssh.execute_command(f'dirb {url} -o {path_out}')
+    #Launch a basic directory enumaration using dirb default wordlist. Return the path of the scan result.
+    def run_dir_enum(self, url: str) -> str:
+        path_out = join(self.results_path, 'dirb_direnum.txt')
+        out = self.ssh.execute_wait_command(f'dirb {url} -o {path_out}')
+        return path_out
 
-    def dir_enum_extension(self, url, path, extensions):
-        path_out = join(path, 'extensions_direnum.dirb')
+    #Launch a directory enumeration with extensions. Return the path of the scan result.
+    def run_dir_enum_extension(self, url: str, extensions: list) -> str:
+        path_out = join(self.results_path, 'dirb_extensions_direnum.txt')
         ext = ','.join(map(lambda e: f'.{e}', extensions))
-        stdin, stdout, stderr = self.ssh.execute_command(f'dirb {url} -X {ext} -o {path_out}')
+        out= self.ssh.execute_wait_command(f'dirb {url} -X {ext} -o {path_out}')
+        return path_out
