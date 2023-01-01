db75bb1ad6daa39b6f3f04fe11a0de9b7de6b7a9
Ferran <ferran.obiols.colomina@gmail.com>
2023-01-01 22:05:24 +0100

    Rework for nikto wrapper (TODO: otuput analyzer)

--- dags/utils/web/nikto.py
+++ dags/utils/web/nikto.py
@@ -3,10 +3,12 @@ from utils.common.ssh import SecureShell
 from os.path import join
 
 class Nikto:
-    def __init__(self, results):
+    def __init__(self, results: str) -> None:
         self.ssh = SecureShell('web-tools', 'root', '/opt/airflow/config/id_rsa')
         self.results_path = results
 
-    def nikto_default(self, url):
-        path_out = join(self.results_path, 'default.nikto')
-        stdin, stdout, stderr = self.ssh.execute_command(f'nikto -nointeractive -o {path_out} -h {url}')
+    #Launch nikto scan to enumerate vulnerabilities. Return the output path.
+    def run_nikto_default(self, url: str) -> str:
+        path_out = join(self.results_path, 'nikto_default.txt')
+        out = self.ssh.execute_wait_command(f'nikto -nointeractive -o {path_out} -h {url}')
+        return path_out
