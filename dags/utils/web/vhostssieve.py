062a2a1148b83124732e8adca267b026724cbcee
Ferran <ferran.obiols.colomina@gmail.com>
2023-01-05 17:55:42 +0100

    Finished vhost-sieve wrapper (TODO: analyze vhost)

--- dags/utils/web/vhostssieve.py
+++ dags/utils/web/vhostssieve.py
@@ -1,16 +1,23 @@
 from utils.common.ssh import SecureShell
 
 from os.path import join
+from urllib.parse import urlparse
 
-class Gobuster:
-    def __init__(self):
+class VhostSieve:
+    def __init__(self, results: str) -> None:
         self.ssh = SecureShell('web-tools', 'root', '/opt/airflow/config/id_rsa')
+        self.results_path = results
 
-    def vhost_enum(self, domain, path):
-        path_out = join(path, 'vhost.gobuster')
+    #Create a temporal wordlist using the domain and launch the vhost-sieve. Return the path of the output.
+    def run_vhost_enum(self, domain: str) -> str:
+        if 'http' in domain:
+            domain = urlparse(domain).netloc
+
+        path_out = join(self.results_path, 'vhost.vhostsieve')
         commands = (
-            f'sed "s/$/.{domain}/" /usr/share/nmap/nselib/data/vhosts-full.lst > /tmp/vhost_domains_{domain}.txt'
+            f'sed "s/$/.{domain}/" /usr/share/nmap/nselib/data/vhosts-full.lst > /tmp/vhost_domains_{domain}.txt',
             f'vhosts-sieve -d /tmp/vhost_domains_{domain}.txt -o {path_out}',
             f'rm /tmp/vhost_domains_{domain}.txt'
         )
         out = self.ssh.execute_wait_commands(commands)
+        return path_out
