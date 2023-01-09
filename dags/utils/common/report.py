754b2e918bd7e0b53e12cb39f1f495ed0c9bbdcf
Ferran <ferran.obiols.colomina@gmail.com>
2023-01-09 22:58:18 +0100

    Add new Reports class and changed the SSH function descriptions

---
+++ dags/utils/common/report.py
@@ -0,0 +1,23 @@
+class Report:
+    def __init__(self, path) -> None:
+        self.results = path
+
+    #TODO
+    def _merge_reports(self):
+        pass
+
+    #TODO
+    def create_json_report(self):
+        reports = self._merge_reports()
+
+    #TODO
+    def create_html_report(self):
+        pass
+
+    #TODO
+    def create_csv_report(self):
+        pass
+
+    #TODO
+    def create_xml_report(self):
+        pass
--- dags/utils/common/ssh.py
+++ dags/utils/common/ssh.py
@@ -3,28 +3,29 @@ from paramiko import SSHClient, RSAKey, AutoAddPolicy
 class SecureShellSingleton(type):
     _instances = {}
 
-    def __call__(self, *args, **kwargs):
+    def __call__(self, *args: tuple, **kwargs: dict):
         if self not in self._instances:
             instace = super().__call__(*args, **kwargs)
             self._instances[self] = instace
         return self._instances[self]
 
 class SecureShell(metaclass=SecureShellSingleton):
-    def __init__(self, host, user, key_path):
+    def __init__(self, host: str, user: str, key_path: str) -> None:
         key = RSAKey.from_private_key_file(key_path)
         self.ssh = SSHClient()
         self.ssh.set_missing_host_key_policy(AutoAddPolicy())
         self.ssh.connect(hostname=host, username=user, pkey=key)
 
-    def execute_command(self, command):
-        return self.ssh.exec_command(command)
+    def execute_command(self, command: str) -> dict:
+        stdin, stdout, stderr = self.ssh.exec_command(command)
+        return {'stdin': stdin, 'stdout': stdout, 'stderr': stderr}
 
-    def execute_wait_command(self, command):
+    def execute_wait_command(self, command: str) -> dict:
         stdin, stdout, stderr = self.ssh.exec_command(command)
         exit_status = stdout.channel.recv_exit_status()
         return {'status': exit_status, 'stdin': stdin, 'stdout': stdout, 'stderr': stderr}
 
-    def execute_commands(self, commands=[]):
+    def execute_commands(self, commands=[]) -> list:
         out = []
         for cmd in commands:
             ssh_stdin, ssh_stdout, ssh_stderr = self.ssh.exec_command(cmd)
@@ -36,7 +37,7 @@ class SecureShell(metaclass=SecureShellSingleton):
             })
         return out
 
-    def execute_wait_commands(self, commands=[]):
+    def execute_wait_commands(self, commands=[]) -> list:
         out = []
         for cmd in commands:
             ssh_stdin, ssh_stdout, ssh_stderr = self.ssh.exec_command(cmd)
@@ -50,5 +51,5 @@ class SecureShell(metaclass=SecureShellSingleton):
             })
         return out
 
-    def close(self):
+    def close(self) -> None:
         self.ssh.close()
