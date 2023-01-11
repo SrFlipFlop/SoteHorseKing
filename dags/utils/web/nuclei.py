592731b122032f8bde8ef5c5807358e3134e368b
Ferran <ferran.obiols.colomina@gmail.com>
2023-01-11 17:57:25 +0100

    Add function descriptions and deleted test class

--- dags/utils/web/nuclei.py
+++ dags/utils/web/nuclei.py
@@ -3,20 +3,24 @@ from utils.common.ssh import SecureShell
 from os.path import join
 
 class Nuclei:
-    def __init__(self, results):
+    def __init__(self, results: str) -> None:
         self.ssh = SecureShell('web-tools', 'root', '/opt/airflow/config/id_rsa')
         self.results_path = results
 
-    def nuclei_default(self, url):
-        path_out = join(self.results_path, 'default.nuclei')
-        stdin, stdout, stderr = self.ssh.execute_command(f'nuclei -fr -o {path_out} -u {url}')
+    #Launch the default nuclei scan. Return the results path
+    def run_nuclei_default(self, url: str) -> str:
+        path_out = join(self.results_pgiath, 'nuclei_default.txt')
+        out = self.ssh.execute_wait_command(f'nuclei -fr -o {path_out} -u {url}')
+        return path_out
 
-    #TODO
-    def nuclei_new_templates(self, url):
-        path_out = join(self.results_path, 'new.nuclei')
-        stdin, stdout, stderr = self.ssh.execute_command(f'nuclei -nt -fr -o {path_out} -u {url}')
-    
-    #TODO
-    def nuclei_cve(self, url):
-        path_out = join(self.results_path, 'vulnerabilities.nuclei')
-        stdin, stdout, stderr = self.ssh.execute_command(f'nuclei -t cves -t vulnerabilities -fr -o {path_out} -u {url}')
+    #Launch a nuclei scan only using new templates. Return the results path
+    def run_nuclei_new_templates(self, url: str) -> str:
+        path_out = join(self.results_path, 'nuclei_new_templates.txt')
+        out = self.ssh.execute_wait_command(f'nuclei -nt -fr -o {path_out} -u {url}')
+        return path_out
+
+    #Launch a nuclei scan only for vulnerabilities and CVE templates. Return the results path
+    def run_nuclei_cve(self, url: str) -> str:
+        path_out = join(self.results_path, 'nuclei_vulnerabilities.txt')
+        out = self.ssh.execute_wait_command(f'nuclei -t cves -t vulnerabilities -fr -o {path_out} -u {url}')
+        return path_out
