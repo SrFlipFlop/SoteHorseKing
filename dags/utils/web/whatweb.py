65b2be018ae04f795bd054931325d172f0fa530a
Ferran <ferran.obiols.colomina@gmail.com>
2023-01-13 18:31:47 +0100

    Finished rework for WhatWeb (TODO: analyze result).

--- dags/utils/web/whatweb.py
+++ dags/utils/web/whatweb.py
@@ -3,16 +3,18 @@ from utils.common.ssh import SecureShell
 from os.path import join
 
 class WhatWeb:
-    def __init__(self, results):
+    def __init__(self, results: str) -> None:
         self.ssh = SecureShell('web-tools', 'root', '/opt/airflow/config/id_rsa')
         self.results_path = results
 
-    def whatweb_default(self, url):
-        path_out = join(self.results_path, 'default.whatweb')
-        path_json = join(self.results_path, 'default_json.whatweb')
-        stdin, stdout, stderr = self.ssh.execute_command(f'whatweb -a 3 --log-verbose {path_out} --log-json {path_json} {url}')
+    def run_find_technologies(self, url: str) -> str:
+        path_out = join(self.results_path, 'whatweb_default.txt')
+        path_json = join(self.results_path, 'whatweb_default.json')
+        out = self.ssh.execute_wait_command(f'whatweb -a 3 --log-verbose {path_out} --log-json {path_json} {url}')
+        return path_json
 
-    def whatweb_aggressive(self, url):
-        path_out = join(self.results_path, 'aggressive.whatweb')
-        path_json = join(self.results_path, 'aggressive_json.whatweb')
-        stdin, stdout, stderr = self.ssh.execute_command(f'whatweb -a 4 --log-verbose {path_out} --log-json {path_json} {url}')
+    def run_find_technologies_aggressive(self, url: str) -> str:
+        path_out = join(self.results_path, 'whatweb_aggressive.txt')
+        path_json = join(self.results_path, 'hatweb_aggressive.json')
+        out = self.ssh.execute_wait_command(f'whatweb -a 4 --log-verbose {path_out} --log-json {path_json} {url}')
+        return path_json
