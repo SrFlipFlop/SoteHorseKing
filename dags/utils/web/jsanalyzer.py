240d5b08b843255c0b2154762447a8ca8dc89fd4
Ferran <ferran.obiols.colomina@gmail.com>
2023-01-14 11:19:35 +0100

    Finished rework for JsAnalyzer (TODO: analyze JS results files)

--- dags/utils/web/jsanalyzer.py
+++ dags/utils/web/jsanalyzer.py
@@ -3,13 +3,15 @@ from utils.common.ssh import SecureShell
 from os.path import join
 
 class JSAnalyzer:
-    def __init__(self, results):
+    def __init__(self, results: str) -> None:
         self.ssh = SecureShell('web-tools', 'root', '/opt/airflow/config/id_rsa')
         self.results_path = results
 
-    def get_javascript(self, url):
+    #Obtain JavaScript files from multiple sources. Return the path of the scan result.
+    def reun_get_javascript(self, url: str) -> str:
         path_out = join(self.results_path, 'getjs.txt')
-        stdin, stdout, stderr = self.ssh.execute_command(f'getJS --complete --url {url} | tee {path_out}')
+        out = self.ssh.execute_wait_command(f'getJS --complete --url {url} | tee {path_out}')
+        return path_out
 
     #TODO
     def analyze_javascript(self, path):
