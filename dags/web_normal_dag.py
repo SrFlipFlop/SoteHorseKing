da8c9331ee0461c92c0f77b7efdc3733014ddb26
Ferran <ferran.obiols.colomina@gmail.com>
2023-04-24 22:48:29 +0200

    Add taks timeout for solving kill tasks

--- dags/web_normal_dag.py
+++ dags/web_normal_dag.py
@@ -1,7 +1,7 @@
 from os import mkdir
 from time import sleep
 from os.path import join, exists
-from datetime import datetime
+from datetime import datetime, timedelta
 from airflow.decorators import dag, task
 
 from utils.web.nmap import Nmap
@@ -33,27 +33,27 @@ from utils.web.vhostssieve import VhostSieve
     },
 )
 def pentest():
-    @task
+    @task(execution_timeout=timedelta(minutes=60))
     def create_results_dir(path):
         #Create results dir from SSH command
-        ssh = SecureShell('web-tools', 'root', '/opt/airflow/config/id_rsa')
-        ssh.execute_command(f'mkdir -p {path}')
+        #ssh = SecureShell('web-tools', 'root', '/opt/airflow/config/id_rsa')
+        #ssh.execute_command(f'mkdir -p {path}')
 
         #Create results dir from normal exeution (check path)
-        #if not exists(path):
-        #    mkdir(path)
+        if not exists(path):
+            mkdir(path)
 
-    @task()
+    @task(execution_timeout=timedelta(minutes=60))
     def wait(delay=0, *args: list, **kwargs: dict):
         if delay:
             sleep(delay)
 
-    @task()
+    @task(execution_timeout=timedelta(minutes=60))
     def find_servers(url: str, path: str, *args: list, **kwargs: dict) -> None:
         nmap = Nmap(path) 
         nmap.run_web_fast(url, ports=[80,3000,8080])
 
-    @task()
+    @task(execution_timeout=timedelta(minutes=60))
     def enum_vhosts(url: str, path: str, *args: list, **kwargs: dict) -> None:
         gobuster = Gobuster(path)
         gobuster.run_vhost_enum(url=url)
@@ -61,41 +61,41 @@ def pentest():
         sieve = VhostSieve(path)
         sieve.run_vhost_enum(url)
 
-    @task()
+    @task(execution_timeout=timedelta(minutes=60))
     def check_web_technology(url: str, path: str, *args: list, **kwargs: dict) -> None:
-        wappy = Wappalyzer(path)
-        wappy.run_find_technologies(url)
+        #wappy = Wappalyzer(path)
+        #wappy.run_find_technologies(url)
 
         whatweb = WhatWeb(path)
-        whatweb.run_find_technologies(url)
+        #whatweb.run_find_technologies(url)
         whatweb.run_find_technologies_aggressive(url)
 
-    @task()
+    @task(execution_timeout=timedelta(minutes=60))
     def check_cms(url: str, path: str, *args: list, **kwargs: dict) -> None:
         seek = CMSeek(path)
         seek.run_cmseek_default(url)
 
-    @task()
+    @task(execution_timeout=timedelta(minutes=60))
     def crawler(url: str, path: str, *args: list, **kwargs: dict) -> None:
         katana = Katana(path)
         katana.run_crawl_full(url)
 
-    @task()
+    @task(execution_timeout=timedelta(minutes=60))
     def javascript(url: str, path: str, *args: list, **kwargs: dict) -> None:
         js = JSAnalyzer(path)
         js.reun_get_javascript(url)
 
-    @task()
+    @task(execution_timeout=timedelta(minutes=60))
     def dir_enum_normal(url: str, path: str, *args: list, **kwargs: dict) -> None:
         gobuster = Gobuster(path)
         gobuster.run_dir_enum(url=url)
 
-    @task()
+    @task(execution_timeout=timedelta(minutes=60))
     def dir_enum_extensions(url: str, path: str, *args: list, **kwargs: dict) -> None:
         dirb = Dirb(path)
-        dirb.run_dir_enum_extension(url=url, extensions=['php', 'txt'])
+        dirb.run_dir_enum_extension(url=url, extensions=['js', 'txt'])
 
-    @task()
+    @task(execution_timeout=timedelta(minutes=60))
     def vulnerability_scanner(url: str, path: str, *args: list, **kwargs: dict) -> None:
         nuclei = Nuclei(path)
         nuclei.run_nuclei_default(url)
@@ -103,12 +103,12 @@ def pentest():
         nikto = Nikto(path)
         nikto.run_nikto_default(url)
 
-    @task()
+    @task(execution_timeout=timedelta(minutes=60))
     def vulnerability_cms_scanner(url: str, path: str, *args: list, **kwargs: dict) -> None:
         wpscan = WPScan(path)
         wpscan.wpscan_default(url)
 
-    @task()
+    @task(execution_timeout=timedelta(minutes=60))
     def server_tls(url: str, path: str, *args: list, **kwargs: dict) -> None:
         testssl = Testssl(path)
         testssl.run_testssl_default(url)
@@ -128,10 +128,11 @@ def pentest():
     step5 = check_cms(URL, PROJECT_PATH, wait1)
     wait2 = wait(5, [step2, step3, step4, step5])
     
-    step6 = crawler(URL, PROJECT_PATH, wait2)
-    step7 = javascript(URL, PROJECT_PATH, wait2)
-    wait3 = wait(5, [step6, step7])
-    
+    #step6 = crawler(URL, PROJECT_PATH, wait2)
+    #step7 = javascript(URL, PROJECT_PATH, wait2)
+    #wait3 = wait(5, [step6, step7])
+    wait3 = wait2
+
     step8 = dir_enum_normal(URL, PROJECT_PATH, wait3)
     step9 = dir_enum_extensions(URL, PROJECT_PATH, wait3)
     wait4 = wait(5, [step8, step9])
